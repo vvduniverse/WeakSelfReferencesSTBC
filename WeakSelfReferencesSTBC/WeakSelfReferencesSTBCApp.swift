c8373097feef8471a64c4401c50cfd5abd4ded58
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-30 16:39:25 +0300

    Initial Commit

---
+++ WeakSelfReferencesSTBC/WeakSelfReferencesSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeakSelfReferencesSTBCApp.swift
+//  WeakSelfReferencesSTBC
+//
+//  Created by Vahtee Boo on 30.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeakSelfReferencesSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
